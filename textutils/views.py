73bb8ea132ede0841a9209d6c2638708df3658b3
rajpipaliya <79788001+rajpipaliya@users.noreply.github.com>
2021-04-02 09:50:48 +0530

    Add files via upload

---
+++ textutils/views.py
@@ -0,0 +1,66 @@
+###################################3###
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def index(request):
+
+    #return HttpResponse('hello')
+    return render(request, 'index.html')
+
+def about(request):
+    return HttpResponse('about')
+
+def analyze(request):
+    #Get the text
+    djtext = request.POST.get('text', 'default')
+
+    # Check checkbox values
+    removepunc = request.POST.get('removepunc', 'off')
+    fullcaps = request.POST.get('fullcaps', 'off')
+    newlineremover = request.POST.get('newlineremover', 'off')
+    extraspaceremover = request.POST.get('extraspaceremover', 'off')
+
+    #Check which checkbox is on
+    if removepunc == "on":
+        punctuations = '''!()-[]{};:'"\,<>./?@#$%^&*_~'''
+        analyzed = ""
+        for char in djtext:
+            if char not in punctuations:
+                analyzed = analyzed + char
+        params = {'purpose':'Removed Punctuations', 'analyzed_text': analyzed}
+        djtext=analyzed
+
+    if(fullcaps=="on"):
+        analyzed = ""
+        for char in djtext:
+            analyzed = analyzed + char.upper()
+
+        params = {'purpose': 'Changed to Uppercase', 'analyzed_text': analyzed}
+        # Analyze the text
+        djtext=analyzed
+
+    if(extraspaceremover=="on"):
+        analyzed = ""
+        for index, char in enumerate(djtext):
+            if not(djtext[index] == " " and djtext[index+1]==" "):
+                analyzed = analyzed + char
+
+        params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
+        # Analyze the text
+        djtext=analyzed
+
+    if (newlineremover == "on"):
+        analyzed = ""
+        for char in djtext:
+            if char != "\n" and char!='\r':                                      ##############
+                analyzed = analyzed + char
+
+        params = {'purpose': 'Removed NewLines', 'analyzed_text': analyzed}
+        # Analyze the text
+
+
+    if (removepunc != "on" and fullcaps!="on" and extraspaceremover!="on" and newlineremover != "on"):
+        return HttpResponse(djtext)
+
+    return render(request, 'analyze.html', params)
